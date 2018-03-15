35be6c96d872f8bd33c42b474bf9a0de0b447b34
HP-Enrique <HP-Enrique@172.16.0.121>
2018-03-15 09:57:16 +0100

    1er Cambio

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -7,6 +7,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		JOptionPane.showMessageDialog(null, "Hola Mundo");
+		
+		System.out.println("Bienvenidos");
 
 	}
 
