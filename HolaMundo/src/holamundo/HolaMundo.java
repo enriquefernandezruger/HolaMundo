62847655f82aaaac1408facca640794c7f9001af
HP-Enrique <HP-Enrique@172.16.0.121>
2018-03-15 09:28:34 +0100

    Estado Inicial

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+import javax.swing.JOptionPane;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		JOptionPane.showMessageDialog(null, "Hola Mundo");
+
+	}
+
+}
